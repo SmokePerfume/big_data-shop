4c0a3e68398d143d66b390403433ffdda37b8d4a
SmokePerfume <wjdgnstls97@naver.com>
2022-03-25 18:27:39 +0900

    누락된 해당 id 멤버 튜플 input에 넣기 추가

---
+++ public/admin/member/member_detail/mem_detail.js
@@ -0,0 +1,8 @@
+//SELECT_ID_MEMBER db에서 받아온 내역
+// const memList=document.getElementById("memList");
+// SELECT_ID_MEMBER.forEach((item)=>{
+//     for(let key in item){
+//         const inputMem=memList.querySelector("."+key);
+//         inputMem.innerText=item[key];
+//     }
+// })
