24699b6bbd295dc95c48e7e923d89bef43fcb036
SmokePerfume <wjdgnstls97@naver.com>
2022-03-27 18:25:01 +0900

    뒤로가기 버튼 구현 나머지 요소들 위치 조정 수정하기 버튼 ui생성 (css)

--- public/admin/member/member_detail/mem_detail.js
+++ public/admin/member/member_detail/mem_detail.js
@@ -1,7 +1,13 @@
+const cancelBtn = document.getElementById("cancelBtn")
+
+cancelBtn.addEventListener("click",()=>{
+    window.location.href='/admin/mem/list/1';
+})
 
 SELECT_ID_MEMBER.forEach((item)=>{
     for(let key in item){
         const inputMem=memForm[key];
-        inputMem.value=item[key]
+        inputMem.value=item[key];
     }
 })
+
